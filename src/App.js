9cdbbd83fce769f1e3703eda759d93dec245ce9f
Himanshu Pandey <hp200229@gmail.com>
2021-06-01 12:23:12 +0530

    added functionality

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import './App.css';
 
 function App() {
   const [image, setImage] = useState()
-  const [brightness, setBrightness] = useState();
-  const [contrast, setContrast] = useState();
-  const [saturate, setSaturate] = useState();
-  const [sepia, setSepia] = useState();
-  const [grayscale, setGrayscale] = useState(false);
-  const [hue, setHue] = useState(false);
-  const [invert, setInvert] = useState(false);
+  const [brightness, setBrightness] = useState(100);
+  const [contrast, setContrast] = useState(100);
+  const [saturate, setSaturate] = useState(100);
+  const [sepia, setSepia] = useState(0);
+  const [blur, setBlur] = useState(0);
+  const [grayscale, setGrayscale] = useState(0);
+  // const [hue, setHue] = useState(0);
+  const [invert, setInvert] = useState(0);
+  const [disable, setDisable] = useState(true)
 
 
   const addImage = e => {
     setImage(URL.createObjectURL(e.target.files[0]))
+    setDisable( false ) 
   }
 
   const removeImage = () => {
@@ -21,7 +24,38 @@ function App() {
     if (choice === true) {
         setImage(null)
     }
+    setDisable( true) 
   }
+
+  const changeBrightness = e => {
+    setBrightness(e.target.value);
+  }
+  const changeContrast = e => {
+    setContrast(e.target.value);
+  }
+  const changeSaturation = e => {
+    setSaturate(e.target.value);
+  }
+  const changeSepia = e => {
+    setSepia(e.target.value);
+  }
+  const changeBlur = e => {
+    setBlur(e.target.value);
+  }
+  const changeGrayscale = e => {
+    setGrayscale(e.target.value);
+  }
+  // const changeHue = e => {
+  //   setHue(e.target.value);
+  // }
+  const changeInvert = e => {
+    setInvert(e.target.value);
+  }
+
+  const style = {
+    filter: `grayscale(${grayscale}%) blur(${blur}px) brightness(${brightness}%) contrast(${contrast}%) saturate(${saturate}%) sepia(${sepia}%) invert(${invert}%)`
+  }
+
   return (
     <div className="App">
       <header>
@@ -33,41 +67,66 @@ function App() {
           <div className="controller-container">
             <p className="name">Brightness</p>
             <div className="input-container">
-              <input type="range" name="" id="" className="inp" />
-              <label htmlFor="">100</label>
+              <input type="range" name="" id="" value={brightness} max="200" min="0" className="inp" onChange={changeBrightness} disabled={disable}/>
+              <label htmlFor="">{brightness}</label>
             </div>
             <p className="name">Contrast</p>
             <div className="input-container">
-              <input type="range" name="" id="" className="inp" />
-              <label htmlFor="">100</label>
+              <input type="range" name="" id="" value={contrast} max="200" min="0" className="inp" onChange={changeContrast} disabled={disable}/>
+              <label htmlFor="">{contrast}</label>
             </div>
             <p className="name">Saturate</p>
             <div className="input-container">
-              <input type="range" name="" id="" className="inp" />
-              <label htmlFor="">100</label>
+              <input type="range" name="" id="" value={saturate} max="200" min="0" className="inp" onChange={changeSaturation} disabled={disable}/>
+              <label htmlFor="">{saturate}</label>
             </div>
             <p className="name">Sepia</p>
             <div className="input-container">
-              <input type="range" name="" id="" className="inp" />
-              <label htmlFor="">100</label>
+              <input type="range" name="" id="" value={sepia} className="inp"  onChange={changeSepia} disabled={disable}/>
+              <label htmlFor="">{sepia}</label>
             </div>
             <p className="name">Blur</p>
             <div className="input-container">
-              <input type="range" name="" id="" className="inp" />
-              <label htmlFor="">100</label>
+              <input type="range" name="" id="" value={blur} className="inp" onChange={changeBlur} disabled={disable}/>
+              <label htmlFor="">{blur}</label>
+            </div>
+            <p className="name">Grayscale</p>
+            <div className="input-container">
+              <input type="range" name="" id="" value={grayscale} className="inp" onChange={changeGrayscale} disabled={disable}/>
+              <label htmlFor="">{grayscale}</label>
+            </div>
+            {/* <p className="name">Hue</p>
+            <div className="input-container">
+              <input type="range" name="" id="" value={hue} className="inp" onChange={changeHue} disabled={disable}/>
+              <label htmlFor="">{hue}</label>
+            </div> */}
+            <p className="name">Invert</p>
+            <div className="input-container">
+              <input type="range" name="" id="" value={invert} className="inp" onChange={changeInvert} disabled={disable}/>
+              <label htmlFor="">{invert}</label>
             </div>
             <div className="btns">
-              <button className="control-btn">Grayscale</button>
-              <button className="control-btn">Hue-Invert</button>
-              <button className="control-btn">Invert</button>
-              <button className="export">Export</button>
+              {/* <button className="control-btn" disabled={disable} onClick={()=>setGrayscale(true)}>Grayscale</button>
+              <button className="control-btn" disabled={disable}>Hue-Invert</button>
+              <button className="control-btn" disabled={disable}>Invert</button> */}
+              <button className="export" disabled={disable}>Export</button>
             </div>
           </div>
         </div>
         <div className="canvas">
           {/* <div className="close">+</div> */}
           {image ? (<button className="close" onClick={removeImage}>+</button>) : null}
-          {image ? (<img src={image} className="image" alt="" />): (<input type="file" id="file" accept="image/*" onChange={addImage} />)}
+          {image ? (
+            <>
+              {/* <p className="filename">{filename}</p> */}
+              <img src={image} style={style} className="image" alt="" />
+            </>
+          ):(
+            <div className="add">
+              <p className="add-label">Add Image File</p>
+              <input type="file" id="file" accept="image/*" onChange={addImage} />
+            </div>
+          )}
         </div>
       </main>
     </div>
